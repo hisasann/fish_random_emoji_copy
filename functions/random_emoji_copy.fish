008b445a485325cfb16e0af7c81d32c0e2252bbe
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-03-09 00:02:29 +0900

    🌭 fix: rename function name to random_emoji_copy

--- functions/random_emoji_copy.fish
+++ functions/random_emoji_copy.fish
@@ -3,7 +3,7 @@ set EMOJI_COUNT (count $EMOJI)
 
 alias pcopy "perl -pe 'chomp' | pbcopy"
 
-function __emoji_random_copy
+function random_emoji_copy
   set RANDOM_EMOJI (random choice $EMOJI)
 
   if type -q pbcopy
